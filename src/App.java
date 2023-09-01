538da3de3804d6655dcd397f56bb3d5229315bd1
omieibih <owharcourt@gmail.com>
2023-09-01 09:51:16 -0500

    Finished Program

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Fraction a = new Fraction(1,3);
+        Fraction b = new Fraction(1,6);
+        Fraction c = new Fraction(1,4);
+        Fraction d = new Fraction(3,6);
+        Fraction e = new Fraction(3,8);
+        Fraction f = new Fraction(2,5);
+        Fraction sum;
+        sum = b.add(e); // 1/6 + 3/8 
+        System.out.println(b + " + " + e + " = " + sum);
+
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+        System.out.println(c.add(d));
+        System.out.println(c.multiply(d));
+        System.out.println(e.add(f));
+        System.out.println(e.multiply(f));
+
+
+
+        
     }
 }
--- src/Fraction.java
+++ src/Fraction.java
@@ -17,14 +17,72 @@ public class Fraction {
 
     public Fraction add(Fraction other)
     {
-        Fraction newFrac = new Fraction();
-        if(this.denominator == other.denominator)
-        {
-            newFrac.numerator = this.numerator + other.numerator;
-            newFrac.denominator = this.denominator;
-        }
-        
-        return newFrac;
+    int n1 = this.numerator;
+    int d1 = this.denominator;
+    int n2 = other.numerator;
+    int d2 = other.denominator;
+    int nfinal = 0;
+    int dfinal = 0; 
 
+    if (d1 != d2)
+    {
+      int lowcommult = Fraction.lcm(d1,d2);
+      int multiplier1 = lowcommult/d1;
+      int multiplier2 = lowcommult/d2;
+      n1 *= multiplier1;
+      n2 *= multiplier2;
+      nfinal = n1 + n2;
+      dfinal = lowcommult;
+      Fraction sumFrac = new Fraction(nfinal, dfinal);
+      sumFrac.reduce();
+      return sumFrac;
     }
+    else {
+      nfinal = n1 + n2;
+      dfinal = d1;
+      Fraction sumFrac = new Fraction(nfinal, dfinal);
+      sumFrac.reduce();
+      return sumFrac;
+    }
+  }
+
+   public Fraction multiply(Fraction other){
+  //variables
+  int n1 = this.numerator;
+  int d1 = this.denominator;
+  int n2 = other.numerator;
+  int d2 = other.denominator;
+  //code
+  int nfinal = n1 * n2;
+  int dfinal = d1 * d2;
+  Fraction newFrac = new Fraction(nfinal,dfinal);
+  newFrac.reduce();
+  return newFrac;
+
+  }
+
+  private void reduce()
+  {
+    int gcd = Fraction.gcd(numerator,denominator);
+    numerator /= gcd;
+    denominator /=gcd;
+  }
+  
+  static int gcd(int a, int b)
+    {
+        if (a == 0)
+            return b;
+        return gcd(b % a, a);
+    }
+     
+    // method to return LCM of two numbers
+    static int lcm(int a, int b)
+    {
+        return (a / gcd(a, b)) * b;
+    }
+
+  public String toString()
+  {
+    return numerator + "/" + denominator;
+  }
 }
